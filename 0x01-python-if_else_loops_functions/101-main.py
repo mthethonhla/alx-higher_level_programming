eb57980a1533a62b5930606a20918f9af64b0d0f
mthethonhla <nhlamthetho@outlook.com>
2024-01-16 19:18:05 -0800

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-main.py
+++ 0x01-python-if_else_loops_functions/101-main.py
@@ -6,10 +6,3 @@ print(remove_char_at("Chicago", 2))
 print(remove_char_at("C is fun!", 0))
 print(remove_char_at("School", 10))
 print(remove_char_at("Python", -2))
-
-guillaume@ubuntu:~/0x01$ ./101-main.py
-Bes School
-Chcago
- is fun!
-School
-Python
