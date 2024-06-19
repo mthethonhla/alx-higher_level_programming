c83b9c10ca3528d4a76105f4e9104d9903055dda
mthethonhla <nhlamthetho@outlook.com>
2024-06-18 19:18:55 -0700

    code style

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 /*  array of string and a loop */
+
 const array = ['C is fun','Python is cool','JavaScript is amazing'];
+
 for (let i = 0; i < array.length; i++) {
   console.log(array[i]);
 }
