1cdc5960df8439f2fafded94542b49692ee6a70c
mthethonhla <nhlamthetho@outlook.com>
2024-06-18 18:32:03 -0700

    semi-standard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-/* prints a message depending on
- * the number of arguments passed
-*/
+/* prints number of arguments passed*/
+
 const args = process.argv;
 if (args.length <= 2) {
   console.log('No argument');
-} else if ( args.length === 3 ) {
+} else if (args.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Argument found');
