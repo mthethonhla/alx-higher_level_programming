7925696e95dfbb95fa630536a26cd77f8a23e2dd
mthethonhla <nhlamthetho@outlook.com>
2024-06-18 18:42:22 -0700

    3-value_argument.js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/bin/bin/node
+/* prints the first argument passed */
+
+const args = process.argv;
+
+if (args[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args[2]);
+}
