a26f7d8f49ad1acd92e91b5d43bb034f9da0ef54
mthethonhla <nhlamthetho@outlook.com>
2024-06-18 18:34:52 -0700

    if statement

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 /* prints number of arguments passed*/
 
 const args = process.argv;
-if (args.length <= 2) {
+if (args.length === 2) {
   console.log('No argument');
 } else if (args.length === 3) {
   console.log('Argument found');
