b98c21779f3db37f29835e282dfa0fba3a30f57e
mthethonhla <nhlamthetho@outlook.com>
2024-06-20 17:59:20 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
-/* prints number of arguments passed*/
 
-const args = process.argv;
-if (args.length === 2) {
-  console.log('No argument');
-} else if (args.length === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Argument found');
-}
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
