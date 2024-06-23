314b37b56d46b8d0d49754709c5f51bee4f2f3b3
mthethonhla <nhlamthetho@outlook.com>
2024-06-23 00:35:08 -0700

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+if __name__ == '__main__':
+
+    # database connection
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+
+    cur.close()
+    db.close()
