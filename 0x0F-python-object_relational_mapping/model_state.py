bbeea717a8d725142392d3081a231a3e787c037d
mthethonhla <nhlamthetho@outlook.com>
2024-06-27 10:40:00 -0700

    fix model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """class definition of a State"""
 
-from sys import argv
+from sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2],argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
