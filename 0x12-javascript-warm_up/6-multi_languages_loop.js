6eef5e8ba3e170820217259db1f3a72c95fe74b3
mthethonhla <nhlamthetho@outlook.com>
2024-06-28 18:47:42 -0700

    dir

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-/*  array of string and a loop */
-
-const array = ['C is fun','Python is cool','JavaScript is amazing'];
-
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
